89a397707c0ce8109a2952b1bcd521b411bcf108
Jiri Stepan <jiri.stepan@etnetera.cz>
2018-03-27 12:41:33 +0200

    zahezceni

--- examples/02-Project/slackAPI.js
+++ examples/02-Project/slackAPI.js
@@ -9,9 +9,9 @@ var moment = require("moment");
 const filesPerPageCount = 100
 
 module.exports = {
-    getAllSlackFiles: getAllSlackFiles,
-    getLargeOldFiles: getLargeOldFiles
+    getLargeOldFiles: getOldLargeFiles
 }
+
 
 var client = axios.create({
     baseURL: 'https://slack.com/api/',
@@ -19,52 +19,6 @@ var client = axios.create({
     headers: { 'Authorization': 'Bearer ' + cfg.slack.token }
 });
 
-function getAllSlackFiles() {
-    return new Promise((resolve, reject) => {
-        debug("Getting all files from SLACK API..")
-        client.get(`files.list?count=${filesPerPageCount}`).then(response => {
-            var paging = response.data.paging;
-            debug("There is %d pages of files", paging.pages)
-            var requests = []
-            for (var i = 1; i <= paging.pages; i++) {
-                var request = client.get(`files.list?count=${filesPerPageCount}&page=${i}`)
-                requests.push(request);
-            }
-            return Promise.all(requests)
-        }).then((results) => {
-            var files = results.map(obj => { return obj.data.files })
-            files = [].concat.apply([], files); //flatten array of arrays
-            resolve(files)
-        }).catch(e => { reject(e) })
-    })
-}
-
-
-function getLargeOldFiles(minSize = 5, minAge = 90) {
-    return getAllSlackFiles()
-        .then((data) => {
-            debug("LENGTH: " + data.length)
-            var files = data.map((obj) => {
-
-                var fileDate = moment(obj.timestamp * 1000).format("YYYY-MM-DD");
-                var today = moment();
-                var duration = moment.duration(today.diff(fileDate));
-                var ageDays = duration.asDays();
-
-                return {
-                    id: obj.id,
-                    name: obj.name + " (" + obj.title + ")",
-                    date: fileDate,
-                    age: ageDays,
-                    size: obj.size / (1024 * 1024),
-                    mode: obj.mode,
-                    mimeType: obj.mimetype,
-                }
-            })
-            var out = files.filter(obj => (obj.size >= minSize) && (obj.age >= minAge));
-            return (out);
-        })
-}
 
 //eslint-disable-next-line no-unused-vars
 function deleteFile(fileId) {
@@ -76,7 +30,52 @@ function deleteFile(fileId) {
     })
 }
 
-//TEST
-getLargeOldFiles(5, 90).then((res) => { debug(res) })
+function getOldLargeFiles(minSize = 10, minAge = 90) {
+    return new Promise((resolve, reject) => {
+        client.get(`files.list?count=${filesPerPageCount}`)
+            .then(response => {
+                var paging = response.data.paging;
+                debug("There is %d pages of files", paging.pages)
+                var requests = []
+                for (var i = 1; i <= paging.pages; i++) {
+                    var request = client.get(`files.list?count=${filesPerPageCount}&page=${i}`)
+                    requests.push(request);
+                }
+                return Promise.all(requests)
+            })
+            .then((results) => {
+                var files = results.map(obj => { return obj.data.files })
+                files = [].concat.apply([], files); //flatten array of arrays
+                return (files)
+            })
+            .then((files) => {
+                debug("Num of files: " + files.length)
+                return files.map((obj) => {
+                    var fileDate = moment(obj.timestamp * 1000).format("YYYY-MM-DD");
+                    var today = moment();
+                    var duration = moment.duration(today.diff(fileDate));
+                    var ageDays = duration.asDays();
 
-//deleteFile("F9VQNUKK8").then((res => debug(res)))
+                    return {
+                        id: obj.id,
+                        name: obj.name + " (" + obj.title + ")",
+                        date: fileDate,
+                        age: ageDays,
+                        size: obj.size / (1024 * 1024),
+                        mode: obj.mode,
+                        mimeType: obj.mimetype,
+                    }
+                })
+            })
+            .then((mappedFiles) => {
+                var out = mappedFiles.filter(obj => (obj.size >= minSize) && (obj.age >= minAge));
+                return (out);
+            })
+            .then(out => { resolve(out) })
+            .catch(e => { reject(e) })
+    })
+}
+
+debug("Start")
+getOldLargeFiles(5, 90).then(out => debug(out))
+debug("end")
