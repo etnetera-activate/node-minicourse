aa96ecf17ee5ac3693a683c5d0bf31912393b533
jiristepan <jiri.stepan@etnetera.cz>
2018-04-08 17:52:30 +0200

    final

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,6 +29,7 @@ module.exports = {
             ref: 'origin/master',
             repo: 'git@github.com:etnetera-activate/node-minicourse.git',
             path: '/home/node/skolenicko',
+            'pre-deploy-local': 'scp examples/02-Project/config.js node@secure.activate.cz:skolenicko/current/examples/02-Project',
             'post-deploy': 'cd examples/02-Project/ && npm install && cd ../../ && pm2 startOrRestart ecosystem.config.js --env production'
         },
     }
