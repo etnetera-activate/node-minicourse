c389f3a7b6dc3e71ddb3090f3d9851f31b91adb6
jiristepan <jiri.stepan@etnetera.cz>
2018-04-08 18:20:14 +0200

    final

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-    /**
-     * Application configuration section
-     * http://pm2.keymetrics.io/docs/usage/application-declaration/
-     */
     apps: [
 
         // First application
@@ -19,10 +15,6 @@ module.exports = {
         }
     ],
 
-    /**
-     * Deployment section
-     * http://pm2.keymetrics.io/docs/usage/deployment/
-     */
     deploy: {
         production: {
             user: 'node',
@@ -30,6 +22,8 @@ module.exports = {
             ref: 'origin/master',
             repo: 'git@github.com:etnetera-activate/node-minicourse.git',
             path: '/home/node/skolenicko',
+
+            "pre-setup": "cd /home/node/skolenicko",
             'pre-deploy-local': 'scp examples/02-Project/config.js node@secure.activate.cz:skolenicko/current/examples/02-Project',
             'post-deploy': 'cd examples/02-Project/ && npm install && cd ../../ && pm2 startOrRestart ecosystem.config.js --env production'
         },
